3f939b6166ff6c77a668ccf2da511b93002b700e
Mahesh <thamerdan@gmail.com>
2022-10-09 16:15:54 +0530

    Another commit

--- newKeyword.js
+++ newKeyword.js
@@ -3,14 +3,20 @@ function createUser(firstName, age) {
   this.age = age;
 }
 
-createUser.prototype.about = function(){
-    console.log(this.firstName,this.age)
-}
-
+createUser.prototype.about = function () {
+  console.log(this.firstName, this.age);
+};
 
 //Creates an object with user details.
-const user1 = new createUser('Mahesh',32); 
-const user2 = new createUser('Eshan',2);
+const user1 = new createUser("Mahesh", 32);
+const user2 = new createUser("Eshan", 2);
 
-user1.about()
-user2.about()
+user1.about();
+user2.about();
+console.log(user1)
+
+for (key in user1){
+  if(user1.hasOwnProperty(key)){
+    console.log(key)
+  }
+}
